5da56c6a4cd61d92a3ec51cac9f3ff505828ef56
Andrey <cdandrey@gmail.com>
2019-08-14 09:11:52 +0300

    Remove debug out

--- src/main.cpp
+++ src/main.cpp
@@ -13,45 +13,20 @@ int main(int, char **)
 			ip_pool.insert({ ipf::get_key(ip_str,'.'),ip_str });
 		}
 
-		//ipf::sort_revers_lex(ip_pool);
-		//ipf::filter1(ip_pool);
-		//ipf::filter4670(ip_pool);
-		//ipf::filter46(ip_pool);
-
 		ipf::ip_list_t ls;
-		ls = std::move(ipf::filter("46.70|86.131",ip_pool));
+		ls = std::move(ipf::filter("....",ip_pool));	// lex reverse sort
 		for (const auto &x : ls)
 			std::cout << x << std::endl;
 
-		ls = std::move(ipf::filter("1...",ip_pool));
+		ls = std::move(ipf::filter("1...",ip_pool));	// filter 1
 		for (const auto &x : ls)
 			std::cout << x << std::endl;
 
-		ls = std::move(ipf::filter("46.70..",ip_pool));
+		ls = std::move(ipf::filter("46.70..",ip_pool)); // filter 46.70
 		for (const auto &x : ls)
 			std::cout << x << std::endl;
 
-		ls = std::move(ipf::filter("46|46|46|46",ip_pool));
-		for (const auto &x : ls)
-			std::cout << x << std::endl;
-
-		ls = std::move(ipf::filter("16.236.0.173",ip_pool));
-		for (const auto &x : ls)
-			std::cout << x << std::endl;
-
-		ls = std::move(ipf::filter("255|0..0",ip_pool));
-		for (const auto &x : ls)
-			std::cout << x << std::endl;
-
-		ls = std::move(ipf::filter("255|0.0|",ip_pool));
-		for (const auto &x : ls)
-			std::cout << x << std::endl;
-
-		ls = std::move(ipf::filter("255.255.1.",ip_pool));
-		for (const auto &x : ls)
-			std::cout << x << std::endl;
-
-		ls = std::move(ipf::filter("5|.154.",ip_pool));
+		ls = std::move(ipf::filter("46|46|46|46",ip_pool)); // filter 46
 		for (const auto &x : ls)
 			std::cout << x << std::endl;
 			
